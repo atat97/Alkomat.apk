3c6d57c28b3d342326e0ba594b9f543ddd8b031f
atat97 <44062128+atat97@users.noreply.github.com>
2019-01-14 21:19:25 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.adrian.alkomat;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     double gramyAlk = 0;
+    String waga1 = "0";
+    Boolean stan;
+    double promile = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Wyczysc(View view){
         gramyAlk = 0;
+        promile = 0;
+        TextView trzezwy = (TextView)findViewById(R.id.trzezwy);
+        trzezwy.setText("");
         WyswietlGramy();
     }
+    public void Inne(View view){
 
+    }
 
     private void WyswietlGramy() {
         TextView IleGram = (TextView)findViewById(R.id.textViewIleGram);
@@ -50,6 +59,59 @@ public class MainActivity extends AppCompatActivity {
 
     private void otworz_proflActivity() {
         Intent intent = new Intent(this, profilActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+    /*private void otworz_proflActivity() {
+        Intent intent = new Intent(this, profilActivity.class);
         startActivity(intent);
+    }*/
+
+
+    /*public void Oblicz(View view) {
+        String waga1 = getIntent().getStringExtra("extra_waga");
+        TextView Obliczone = (TextView)findViewById(R.id.textViewOblicz);
+        int wagai = Integer.parseInt(waga1);
+        double promile = gramyAlk/(0.6*wagai);
+        Obliczone.setText(String.format("%.2f", promile));
+
+    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                Bundle extras = data.getExtras();
+                waga1 = extras.getString("extra_waga");
+                stan = extras.getBoolean("extra_stan");
+                //Toast.makeText(MainActivity.this, "Stan: " + stan, Toast.LENGTH_SHORT ).show();
+                //Toast.makeText(MainActivity.this, "Waga: " + waga1, Toast.LENGTH_SHORT ).show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+            }
+        }
+    }
+    public void Oblicz(View view){
+        TextView Obliczone = (TextView)findViewById(R.id.textViewOblicz);
+        int wagai = Integer.parseInt(waga1);
+        if(wagai == 0) {
+            Toast.makeText(MainActivity.this, "BRAK WAGI!", Toast.LENGTH_SHORT ).show();
+        } else {
+            if (stan == Boolean.TRUE)
+                promile = gramyAlk/(0.7*wagai);
+            else
+                promile = gramyAlk/(0.6*wagai);
+        }
+        Obliczone.setText(String.format("%.2f", promile));
+
+        double petla = promile;
+        int godz = 0;
+        while (petla >0.2) {
+            petla = petla - 0.12;
+            godz = godz + 1;
+        }
+        godz = godz - 1;
+        TextView trzezwy = (TextView)findViewById(R.id.trzezwy);
+        trzezwy.setText(String.format("Bedziesz trzeźwy/a za ok. %dh",godz));
     }
 }
--- profilActivity.java
+++ profilActivity.java
@@ -1,14 +1,23 @@
 package com.example.adrian.alkomat;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class profilActivity extends AppCompatActivity {
 
-    public static final String EXTRA_WAGA = "com.example.adrian.alkomat.EXTRA_WAGA";
+    Boolean stan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +25,33 @@ public class profilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profil);
     }
 
-    public void zapiszProfil(View view) {
+    /*public void zapiszProfil(View view) {
         TextView Waga = (TextView)findViewById(R.id.waga);
-        int waga_int = Integer.parseInt(Waga.getText().toString());
+        String waga = Waga.getText().toString();
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(EXTRA_WAGA, waga_int);
+
+        Intent intent = new Intent(profilActivity.this, MainActivity.class);
+        intent.putExtra("extra_waga",waga);
+        //Toast.makeText(profilActivity.this, "Waga: " + waga, Toast.LENGTH_SHORT ).show();
         startActivity(intent);
+    }*/
+    public void zapiszProfil(View view){
+        Intent returnIntent = new Intent();
+
+
+        TextView Waga = (TextView) findViewById(R.id.waga);
+        String waga = Waga.getText().toString();
+
+        returnIntent.putExtra("extra_waga",waga);
+
+        Switch plec = (Switch) findViewById(R.id.switch1plec);
+        stan = plec.isChecked();
+        returnIntent.putExtra("extra_stan",stan);
+
+        setResult(Activity.RESULT_OK,returnIntent);
+
+        //Toast.makeText(profilActivity.this, "Waga: " + waga, Toast.LENGTH_SHORT ).show();
+        finish();
     }
 }
+
